29c90c067571c5d655126040c6eb70d10dabebd7
Pravin Pushkar <ppushkar@microsoft.com>
2022-11-07 13:46:11 +0530

    Separate version and image variant column
    Signed-off-by: Pravin Pushkar <ppushkar@microsoft.com>

--- pkg/kubernetes/status.go
+++ pkg/kubernetes/status.go
@@ -41,14 +41,15 @@ type StatusClient struct {
 
 // StatusOutput represents the status of a named Dapr resource.
 type StatusOutput struct {
-	Name      string `csv:"NAME"`
-	Namespace string `csv:"NAMESPACE"`
-	Healthy   string `csv:"HEALTHY"`
-	Status    string `csv:"STATUS"`
-	Replicas  int    `csv:"REPLICAS"`
-	Version   string `csv:"VERSION"`
-	Age       string `csv:"AGE"`
-	Created   string `csv:"CREATED"`
+	Name         string `csv:"NAME"`
+	Namespace    string `csv:"NAMESPACE"`
+	Healthy      string `csv:"HEALTHY"`
+	Status       string `csv:"STATUS"`
+	Replicas     int    `csv:"REPLICAS"`
+	Version      string `csv:"VERSION"`
+	ImageVariant string `csv:"IMAGE-VARIANT"`
+	Age          string `csv:"AGE"`
+	Created      string `csv:"CREATED"`
 }
 
 // Create a new k8s client for status commands.
@@ -97,6 +98,12 @@ func (s *StatusClient) Status() ([]StatusOutput, error) {
 			age := age.GetAge(pod.CreationTimestamp.Time)
 			created := pod.CreationTimestamp.Format("2006-01-02 15:04.05")
 			version := image[strings.LastIndex(image, ":")+1:]
+			imageVariant := ""
+			if strings.Contains(version, "-") {
+				imageVariant = version[strings.Index(version, "-")+1:]
+				version = version[:strings.Index(version, "-")]
+			}
+
 			status := ""
 
 			// loop through all replicas and update to Running/Healthy status only if all instances are Running and Healthy.
@@ -129,14 +136,15 @@ func (s *StatusClient) Status() ([]StatusOutput, error) {
 			}
 
 			s := StatusOutput{
-				Name:      label,
-				Namespace: namespace,
-				Created:   created,
-				Age:       age,
-				Status:    status,
-				Version:   version,
-				Healthy:   healthy,
-				Replicas:  replicas,
+				Name:         label,
+				Namespace:    namespace,
+				Created:      created,
+				Age:          age,
+				Status:       status,
+				Version:      version,
+				ImageVariant: imageVariant,
+				Healthy:      healthy,
+				Replicas:     replicas,
 			}
 
 			m.Lock()
